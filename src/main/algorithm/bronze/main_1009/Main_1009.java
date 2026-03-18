c54cfbdeca2003804dab68050505988b2a1fcf03
min jae Cho <alswodi5@gmail.com>
2026-03-18 21:37:07 +0900

    first commit

---
+++ src/main/algorithm/bronze/main_1009/Main_1009.java
@@ -0,0 +1,32 @@
+package algorithm.bronze.main_1009;
+
+import java.util.Scanner;
+
+public class Main_1009 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = scanner.nextInt();
+
+        for (int i = 0; i < counter; i++) {
+
+            int integerA = scanner.nextInt() % 10;
+            int integerB = scanner.nextInt();
+
+            int exponent = integerB % 4;
+
+            if (exponent == 0) {
+                exponent = 4;
+            }
+
+            int result = (int) Math.pow(integerA, exponent) % 10;
+
+            if (result == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(result);
+            }
+        }
+    }
+}
---
+++ src/main/java/algorithm/debug/DebugPractice.java
@@ -0,0 +1,40 @@
+package algorithm.debug;
+
+public class DebugPractice {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("합계: " + getSum(numbers));
+        System.out.println("평균: " + getAverage(numbers));
+        System.out.println("최댓값: " + getMax(numbers));
+    }
+
+    // 합계 구하기
+    static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i <= numbers.length; i++) { // 버그 1
+            sum += numbers[i];
+        }
+        return sum;
+    }
+
+    // 평균 구하기
+    static double getAverage(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length; // 버그 2
+    }
+
+    // 최댓값 구하기
+    static int getMax(int[] numbers) {
+        int max = 0; // 버그 3
+        for (int number : numbers) {
+            if (number > max) {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
